19eb13fa6955935e0946559cec7a262de62dfaf1
MoJoVi <kezikov.maxim@gmail.com>
2019-04-09 13:45:59 +0500

    Made more readable

--- euler10.py
+++ euler10.py
@@ -1,16 +1,16 @@
-#Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
+# Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
 
-#Найдите сумму всех простых чисел меньше двух миллионов.
-n=0
+# Найдите сумму всех простых чисел меньше двух миллионов.
+n = 0
 primes = [3]
 for i in range(5, 2000000, 2):
     if primes[n] ** 2 == i:
         n += 1
         continue
-    for k in primes[:n+1]:#divider
+    for k in primes[:n + 1]:
         if i % k == 0:
             break
         elif k == primes[n]:
             primes.append(i)
             break
-print(sum(primes)+2)
+print(sum(primes) + 2)
--- euler4.py
+++ euler4.py
@@ -1,17 +1,22 @@
-#Число-палиндром с обеих сторон (справа налево и слева направо) читается одинаково.
-#Самое большое число-палиндром, полученное умножением двух двузначных чисел – 9009 = 91 × 99.
+# Число-палиндром с обеих сторон (справа налево и слева направо)
+# читается одинаково.
+# Самое большое число-палиндром, полученное умножением
+# двух двузначных чисел – 9009 = 91 × 99.
 
-#Найдите самый большой палиндром, полученный умножением двух трехзначных чисел.
+# Найдите самый большой палиндром, полученный умножением
+# двух трехзначных чисел.
 d = 0
-i = 998000#the largest number that can be obtained by multiplying three-digit numbers
+i = 998000  # the largest number that can be obtained by
+# multiplying three-digit numbers
 while d == 0:
-    i -=1
-    if str(i) == str(i)[::-1]:#find the most bigger palindrome
-        for d2 in range(999,100,-1):#check whether you can get this number by
-            #multiplication of three-digit numbers
-            if i / d2 >999:
+    i -= 1
+    if str(i) == str(i)[::-1]:  # find the most bigger palindrome
+        for d2 in range(999, 100, -1):
+            # check whether you can get this number by
+            # multiplication of three-digit numbers
+            if i / d2 > 999:
                 break
             if i % d2 == 0:
-                d = int(i/d2)
+                d = int(i / d2)
                 break
-print(d,d2,i)
+print(d, d2, i)
