d1777086ec8c198d0c50a2070736069f904062ef
MoJoVi <kezikov.maxim@gmail.com>
2019-06-05 02:14:52 +0500

    Reformatted

--- euler4.py
+++ euler4.py
@@ -1,22 +1,21 @@
-# Число-палиндром с обеих сторон (справа налево и слева направо)
-# читается одинаково.
-# Самое большое число-палиндром, полученное умножением
-# двух двузначных чисел – 9009 = 91 × 99.
+"""Число-палиндром с обеих сторон (справа налево и слева направо)
+читается одинаково.
+Самое большое число-палиндром, полученное умножением
+двух двузначных чисел – 9009 = 91 × 99.
 
-# Найдите самый большой палиндром, полученный умножением
-# двух трехзначных чисел.
-d = 0
-i = 998000  # the largest number that can be obtained by
-# multiplying three-digit numbers
-while d == 0:
-    i -= 1
-    if str(i) == str(i)[::-1]:  # find the most bigger palindrome
-        for d2 in range(999, 100, -1):
-            # check whether you can get this number by
-            # multiplication of three-digit numbers
-            if i / d2 > 999:
+Найдите самый большой палиндром, полученный умножением
+двух трехзначных чисел."""
+res = 0
+limit = 999 * 999
+for num in range(limit, 0, -1):
+    if str(num) == str(num)[::-1]:
+        for div in range(999, 100, -1):
+
+            if num / div > 999:
                 break
-            if i % d2 == 0:
-                d = int(i / d2)
+            if num % div == 0:
+                res = num
                 break
-print(d, d2, i)
+    if res:
+        break
+print(res)
--- practice.py
+++ practice.py
@@ -1,52 +1,270 @@
-import time
-start_time = time.time()
-n = '''
-    73167176531330624919225119674426574742355349194934
-    96983520312774506326239578318016984801869478851843
-    85861560789112949495459501737958331952853208805511
-    12540698747158523863050715693290963295227443043557
-    66896648950445244523161731856403098711121722383113
-    62229893423380308135336276614282806444486645238749
-    30358907296290491560440772390713810515859307960866
-    70172427121883998797908792274921901699720888093776
-    65727333001053367881220235421809751254540594752243
-    52584907711670556013604839586446706324415722155397
-    53697817977846174064955149290862569321978468622482
-    83972241375657056057490261407972968652414535100474
-    82166370484403199890008895243450658541227588666881
-    16427171479924442928230863465674813919123162824586
-    17866458359124566529476545682848912883142607690042
-    24219022671055626321111109370544217506941658960408
-    07198403850962455444362981230987879927244284909188
-    84580156166097919133875499200524063689912560717606
-    05886116467109405077541002256983155200055935729725
-    71636269561882670428252483600823257530420752963450
-'''
+# #*ЗАДАЧИ НА ДЕКОРАТОРЫ
+#
+# #Написать декоратор, который отменяет выполнение любой декорированной
+# #функций и будет писать в консоль: ИМЯ_ФУНКЦИИ is canceled!
+#
+# def is_closing(function):
+#     def inner(*arg):
+#         try:
+#             raise SyntaxError
+#         except:
+#             print(function.__name__,'is closing!')
+#     return inner
+#
+# @is_closing
+# def funk(arg):
+#     print('Function is work!', arg)
+#
+# @is_closing
+# def func(*args):
+#     return print(sum(args))
+#
+# funk('hipe')
+# func(6,78,9,6)
+#
+# #Реализовать декоратор, который измеряет скорость выполнения функций.
+# import time
+# start_time = time.time()
+#
+# def timer(func):
+#     def inner(*args):
+#         x = func(*args)
+#         print('{} seconds'.format(time.time()- start_time))
+#         return x
+#     return inner
+#
+# @timer
+# def funk(arg):
+#     print('Function is work!', arg)
+#
+# @timer
+# def summ(*args):
+#     return sum(args)
+#
+# print(summ(5,6,453545,345,34,56,456,45,64,56,456,45))
+# funk('NINI')
+#
+# #Реализовать декоратор, который будет считать, сколько раз выполнялась функция
+#
+# def counter(func):
+#     n = 0
+#     def inner(*args):
+#         nonlocal n
+#         n += 1
+#         print('Function {} wos running {} times'.format(func.__name__,n))
+#         return func(*args)
+#     return inner
+#
+# @counter
+# def funk(arg):
+#     print('Function is work!', arg)
+#
+# @counter
+# def summ(*args):
+#     return sum(args)
+#
+# print(summ(16,35,664,4,6,4658,4))
+# funk('mom')
+# funk('mom')
+# print(summ(165,65,51,6,432,16,4))
+# print(summ(165,65,51,6,432,16,4))
+# funk('mom')
+# funk('mom')
+# print(summ(165,65,51,6,432,16,4))
+# print(summ(165,65,51,6,432,16,4))
+# funk('mom')
+# print(summ(165,65,51,6,432,16,4))
+# print(summ(165,65,51,6,432,16,4))
+#
+# #Реализовать декоторатор, который будет логгировать процесс выполнения функции:
+# #создан декоратор, начато выполнение функции, закончено выполнение функции
+#
+# def decorator(function):
+#     def inner(*arg):
+#         print('Running Decorator...\n Snacks Function {}'.format(function.__name__))
+#         x = function(*arg)
+#         print('The Function {} is completed...\n Closing Decorator'.format(function.__name__))
+#         return x
+#     return inner
+#
+# @decorator
+# def funk(arg):
+#     print('Function is work!', arg)
+#
+# @decorator
+# def summ(*args):
+#     return sum(args)
+#
+# print(summ(16,35,664,4,6,4658,4))
+# funk('mom')
+#
+# #Реализовать декоратор, который будет перехватывать все исключения в функции.
+# #Если они случились, нужно просто писать в консоль сообщение об ошибке
+# def errors_hunter(function):
+#     def inner(*arg):
+#         try:
+#             x = function(*arg)
+#         except IndexError as e:
+#             print(e)
+#         except SyntaxError as e:
+#             print(e)
+#         except TypeError as e:
+#             print(e)
+#         except NameError as e:
+#             print(e)
+#         else:
+#             return x
+#     return inner
+#
+# @errors_hunter
+# def funk(arg):
+#     print('Function is work!', arg)
+#
+# @errors_hunter
+# def summ(*args):
+#     return sum(args)
+#
+# print(summ(16,35,664,4,6,print(),4))
+# print(summ(16,35,664,4,6,'rrr',4))
+# print(summ(None))
+# funk('mom', None)
+#
+# #ЗАДАЧИ НА MAP/FILTER/REDUCE (И LAMBDA, ЕСЛИ НУЖНО)
+#
+# #При помощи map посчитать остаток от деление на 5 для чисел: [1, 4, 5, 30, 99]
+#
+# x = list(map(lambda y: y % 5, [1, 4, 5, 30, 99]))
+# print(x)
+#
+# #При помощи map превратить все числа из массива [3, 4, 90, -2] в строки
+#
+# x = list(map(str, [3, 4, 90, -2]))
+# print(x)
+#
+# #При помощи filter убрать из массива ['some', 1, 'v', 40, '3a', str] все строки
+#
+# x = list(filter(lambda y: type(y) != str, ['some', 1, 'v', 40, '3a', str]))
+# print(x)
+#
+# import math
+#
+# import time
+# start_time = time.time()
+#
+#
+#
+# end_time=time.time()-start_time
+# print(end_time)
+#
+# #написать генератор, который, при его вызове,
+# #будет возвращать случайное значение
+#
+# import random
+# def rand_numb():
+#     while True:
+#         yield random.randint(1,10)
+# x = rand_numb()
+#
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+#
+# #написать генератор который будет работать как range()
+# def gen_range(arg):
+#     n = 0
+#     if type(arg) == str:
+#         while True:
+#             yield arg[n]
+#             n += 1
+#     elif type(arg) == int:
+#         while n < arg:
+#             yield n
+#             n += 1
+#     else:
+#         while True:
+#             yield arg[n]
+#             n += 1
+# x = gen_range('abc')
+# y = gen_range(5)
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+#
+# #Написать генератор, который работает как map
+#
+# def gen_map(func, *args):
+#     n = 0
+#     while True:
+#         yield func(args[n])
+#         n += 1
+#
+# x = gen_map(str, 1,2,3,4,5,6)
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+#
+#
+# #написать генератор который работает как 'enumerate()'
+#
+# def gen_enum(arg):
+#     n = 0
+#     if type(arg) == str:
+#         while True:
+#             yield arg[n], n
+#             n += 1
+#     else:
+#         while True:
+#             yield arg[n], n
+#             n += 1
+# x = gen_enum('qwe')
+# y = gen_enum([1,2,3])
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+#
+# #написать генератор который работает как 'zip()'
+#
+# def gen_zip(list1, list2):
+#     n = 0
+#     while True:
+#         yield (list1[n], list2[n])
+#         n += 1
+# x = gen_zip([1,2,3], (4,5,6))
+# print(next(x))
+# print(next(x))
+# print(next(x))
 
-c = 13
+def dec(func):
+    # func.counter = 0
 
-n = n.replace('\n', '').replace(' ', '')
+    def fake(value):
+        # func.counter += 1
+        fake.counter += 1
+        # print('Runs: ', func.counter)
+        print('Runs:', fake.counter)
+        return 'fake: ' + func(value)
 
-m = 0
+    fake.counter = 0
+    print('Decorated')
+    return fake
 
-for i in n.split('0'):
+@dec
+def my_func(value):
+    return str(value)
 
-    if len(i) < c:
-        continue
+# a = dec(my_func)
+print(my_func(123))
+print(my_func([]))
+print(my_func({}))
 
-    p = 1
-    for j in i[0: c]:
-        p *= int(j)
-
-    if m < p:
-        m = p
-
-    for j in range(c, len(i)):
-        p //= int(i[j - c])
-        p *= int(i[j])
-
-        if m < p:
-            m = p
-
-print(m)
-print("--- %s seconds ---" % (time.time() - start_time))
