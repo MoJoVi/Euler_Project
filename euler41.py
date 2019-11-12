fb313e5605b2c667ccd066cd8f8892fde26f026d
MoJoVi <kezikov.maxim@gmail.com>
2019-11-12 22:37:23 +0500

    added 27, 29, 30

---
+++ euler41.py
@@ -0,0 +1,21 @@
+"""
+Будем считать n-значное число пан-цифровым, если каждая из цифр от 1 до
+n используется в нем ровно один раз. К примеру, 2143 является 4-значным
+пан-цифровым числом, а также простым числом.
+
+Какое существует наибольшее n-значное пан-цифровое простое число?
+"""
+from itertools import permutations
+from euler7 import is_prime
+
+res = 0
+for i in range(9):
+    for num in permutations('987654321'[i:], 9 - i):
+        num = int(''.join(num))
+        if is_prime(num) and num % 2:
+            res = num
+            break
+    if res:
+        break
+
+print(res)
---
+++ euler46.py
@@ -0,0 +1,32 @@
+"""
+Кристиан Гольдбах показал, что любое нечетное составное число можно
+записать в виде суммы простого числа и удвоенного квадрата.
+
+9 = 7 + 2×1^2
+15 = 7 + 2×2^2
+21 = 3 + 2×3^2
+25 = 7 + 2×3^2
+27 = 19 + 2×2^2
+33 = 31 + 2×1^2
+
+Оказалось, что данная гипотеза неверна.
+
+Каково наименьшее нечетное составное число, которое нельзя записать в
+виде суммы простого числа и удвоенного квадрата?
+"""
+from euler7 import is_prime
+
+
+def checker(num):
+    first_nums = [2] + [n for n in range(3, num - 1, 2) if is_prime(n)]
+    for first in first_nums:
+        for second in range(int(num ** 0.5)):
+            if first + 2 * (second ** 2) == num:
+                return False
+    return True
+
+
+for num in (num for num in range(11, 10 ** 4, 2) if not is_prime(num)):
+    if checker(num):
+        print(num)
+        break
